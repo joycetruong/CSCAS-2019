5daf4561d1d2838f336531a2ec99922247836695
Lakpa Finju Sherpa <connectedtofinju1@gmail.com>
2019-10-22 17:19:38 -0700

    i did some changes

---
+++ VancouverPublicLibrary/src/img/Tryit Editor v3.6_files/snhbMLSettings.js
@@ -0,0 +1,30 @@
+snhb.modules.machineLearning.mlSettings = {
+	bottom_medium_rectangle: {
+		mean: 0.090021,
+		dev: 0.015000
+	},
+	try_it_leaderboard: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	right_bottom_medium_rectangle: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	mid_content: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	wide_skyscraper: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	main_leaderboard: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	sidebar_sticky: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+};
