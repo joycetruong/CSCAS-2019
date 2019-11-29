2bfab0f7fb5b9739bff7004272e7f57f9ba2084f
AalyanArif <56889272+AalyanArif@users.noreply.github.com>
2019-11-29 15:03:08 -0800

    dfsd

---
+++ Portfolio/Arrays.py
@@ -0,0 +1,14 @@
+#Ask user for 10 values, print highest
+#AALYAN Arif
+#8th October
+
+
+numbers = []
+num= int(input("Enter amount of numbers: "))
+for i in range(1, num+1):
+  numb =int(input(" Enter the Fucking Numbers!! "))
+  numbers.append(numb)
+print("Largest element is", max(numbers),("bud"))
+
+
+
